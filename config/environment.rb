001997e7b000dca90d135dedd87ed360559d98dc
joshkestenberg <joshkestenberg@gmail.com>
2017-03-22 16:03:13 -0400

    added smtp settings in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :user_name            => ENV["EMAIL"],
