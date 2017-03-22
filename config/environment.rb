49ddee83ec961cc8dc71886fabac9d9ab70d3e6a
joshkestenberg <joshkestenberg@gmail.com>
2017-03-22 16:12:39 -0400

    screwing with env variables please god work

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => EMAIL,
-  :password             => PASSWORD,
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => 587,
+#   :user_name            => EMAIL,
+#   :password             => PASSWORD,
+#   :authentication       => "plain",
+#   :enable_starttls_auto => true
+# }
